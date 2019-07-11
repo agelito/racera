82a13671f40b79694bd229124c8b5033c8157e28
Axel Wettervik <axel.wettervik@gmail.com>
2019-07-11 23:34:17 +0900

    Add OpenGL includes. Fix build script.

--- build.bat
+++ build.bat
@@ -12,9 +12,10 @@ if not exist obj (
 
 set SRC_FILES=src/win32_main.c src/racera_game.c src/math.c src/opengl.c src/renderer.c src/shader.c src/mesh.c src/texture.c src/keyboard.c
 set LIBRARIES=kernel32.lib gdi32.lib user32.lib opengl32.lib
+set INCLUDE_DIR=lib/include
 
 echo "Compiling..."
-cl -EHa- /Od /Zi /W3 /D_CRT_SECURE_NO_WARNINGS /Foobj/ /Febin/racera.exe %SRC_FILES% %LIBRARIES% /link /subsystem:console
+cl -EHa- /Od /Zi /W3 /D_CRT_SECURE_NO_WARNINGS /Foobj/ /Febin/racera.exe /I %INCLUDE_DIR% %SRC_FILES% %LIBRARIES% /link /subsystem:console
 
 echo "Copying Game Assets..."
 xcopy assets\* bin\ /v /d /y /s /e /f
